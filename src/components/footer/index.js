a78d0ea28d6f17c056909ddcb364698e251419a4
reconcubed <recongamingsam@gmail.com>
2020-05-21 18:02:01 +1000

    fixed footer not appearing in production

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,14 +2,14 @@ import { h } from 'preact';
 import style from './style.css';
 
 const Footer = () => (
-	<div className={style.footer}>
+	<footer className={style.footer}>
 		<div>
 			<small>Sam</small>
 		</div>
 		<div>
 			<a href={'https://github.com/reconcubed'}><small>github</small></a> | <a href={'https://medium.com/reconaissance'}><small>medium</small></a>
 		</div>
-	</div>
+	</footer>
 );
 
 export default Footer;
